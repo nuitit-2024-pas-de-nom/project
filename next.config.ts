600ba9fd55225a39233b91c3dc73640c0db6e9e9
Dinholu <alizeehett@hotmail.fr>
2024-12-05 22:43:53 +0100

    Config netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // Vos options ici
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
