7c030b4e0e60e15b9037beb8183176f0f2a26d89
Alexandre <aofferle@cfai-formation.fr>
2024-12-05 23:57:00 +0100

    Initialize Layout with DaisyUI and themes.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,21 +2,20 @@ import type { Config } from "tailwindcss";
 
 import daisyui from "daisyui";
 
+import generated from "@tailwindcss/typography";
+
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
   plugins: [
+      generated,
       daisyui,
   ],
+  daisyui: {
+    themes: ["dark", "light", "business"],
+  }
+
 } satisfies Config;
