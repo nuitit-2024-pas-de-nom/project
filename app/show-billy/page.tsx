9841277622aee45ba1b8a4354986f28edd3e809d
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-12-06 02:46:13 +0100

    ✨ Add dynamic pages home pages edge 😏😏😏

--- app/show-billy/page.tsx
+++ app/show-billy/page.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
+import {healthProblems} from "@/data/health-problems";
 
 export default function show() {
     return (
         <div>
             <h1>show</h1>
-            <Link href={"/heart"} className={"btn btn-primary"}>Heart</Link>
-            <Link href={"/liver"} className={"btn btn-primary"}>Liver</Link>
-            <Link href={"/zeub"} className={"btn btn-primary"}>Zeub</Link>
-            <Link href={"/head"} className={"btn btn-primary"}>Head</Link>
-
+            {healthProblems.map((problem) => (
+                <div key={problem.position}>
+                    <Link href={`/${problem.endpoint}`} className="btn btn-primary">
+                        {problem.title}
+                    </Link>
+                </div>
+            ))}
         </div>
     )
 }
